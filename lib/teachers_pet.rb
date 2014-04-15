cc5dcb798baa44993340d4c75d43a96987c48550
Aidan Feldman <afeld@github.com>
2014-04-15 18:56:53 -0400

    set up first naieve spec, for clone_repos

--- lib/teachers_pet.rb
+++ lib/teachers_pet.rb
@@ -1,2 +1,4 @@
+Dir.glob(File.join(File.dirname(__FILE__), '**', '*.rb'), &method(:require))
+
 module TeachersPet
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@
 
 require 'webmock/rspec'
 
+require_relative File.join('..', 'lib', 'teachers_pet')
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
