0132898de609638284029a43a7410337a9fc52b1
Aidan Feldman <afeld@github.com>
2014-06-04 18:06:21 -0400

    convert open_issue to CLI

--- lib/teachers_pet/actions/interactive.rb
+++ lib/teachers_pet/actions/interactive.rb
@@ -46,14 +46,8 @@ module TeachersPet
         `#{command}`
       end
 
+
       protected
-      def repository?(organization, repo_name)
-        begin
-          @client.repository("#{organization}/#{repo_name}")
-        rescue
-          return false
-        end
-      end
 
       def get_students_file_path
         ask("What is the filename of the list of students?") { |q| q.default = TeachersPet::Configuration.studentsFile }
@@ -61,63 +55,6 @@ module TeachersPet
 
       def get_instructors_file_path
         ask("What is the filename of the list of instructors?") { |q| q.default = TeachersPet::Configuration.instructorsFile }
-      end
-
-      def get_existing_repos_by_names(organization)
-        repos = Hash.new
-        response = @client.organization_repositories(organization)
-        print " Org repo names"
-        response.each do |repo|
-          repos[repo[:name]] = repo
-          print " '#{repo[:name]}'"
-        end
-        print "\n";
-        return repos
-      end
-
-      def get_teams_by_name(organization)
-        org_teams = @client.organization_teams(organization)
-        teams = Hash.new
-        org_teams.each do |team|
-          teams[team[:name]] = team
-        end
-        return teams
-      end
-
-      def get_team_member_logins(team_id)
-        @client.team_members(team_id).map do |member|
-          member[:login]
-        end
-      end
-
-      def read_file(filename, type)
-        map = Hash.new
-        puts "Loading #{type}:"
-        File.open(filename).each_line do |team|
-          # Team can be a single user, or a team name and multiple users
-          # Trim whitespace, otherwise issues occur
-          team.strip!
-          items = team.split(' ')
-          items.each do |item|
-            abort("No users can be named 'owners' (in any case)") if 'owners'.eql?(item.downcase)
-          end
-
-          if map[items[0]].nil?
-            map[items[0]] = Array.new
-            puts " -> #{items[0]}"
-            if (items.size > 1)
-              print "  \\-> members: "
-              1.upto(items.size - 1) do |i|
-                print "#{items[i]} "
-                map[items[0]] << items[i]
-              end
-              print "\n"
-            else
-              map[items[0]] << items[0]
-            end
-          end
-        end
-        return map
       end
 
       def confirm(message, abort_on_no = true)
--- spec/actions/open_issue_spec.rb
+++ spec/actions/open_issue_spec.rb
@@ -1,64 +1,65 @@
 require 'spec_helper'
 
 describe TeachersPet::Actions::OpenIssue do
-  include InteractiveHelpers
+  include CliHelpers
 
-  let(:action) { TeachersPet::Actions::OpenIssue.new }
+  context 'through CLI' do
+    def common_test(labels)
+      issue_title = "Issue Test"
+      request_stubs = []
 
-  def common_test(labels)
-    issue_title = "Issue Test"
-
-    # Respond to action prompts
-    respond("What repository will the issue be raised in?", 'testrepo')
-    respond("What is the organization name?", 'testorg')
-    respond("What is the title of the issue?", 'Issue Test')
-    respond("What is the path to the file containing the issue body?", issue_fixture_path)
-    respond("Optionally add any labels, seperated by commas:", labels)
-    respond("What is the filename of the list of students?", students_list_fixture_path)
-    respond("What is the filename of the list of instructors?", instructors_list_fixture_path)
-    respond("Create issue '#{issue_title}' in #{File.open(students_list_fixture_path).readlines.size} student repositories - 'testrepo'?", "1")
-    stub_github_config
-
-    request_stubs = []
-
-    request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg',
-      login: 'testorg',
-      url: 'https://api.github.com/orgs/testorg'
-    )
-    request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg/teams?per_page=100', student_teams)
-    student_usernames.each do |username|
-      # Action checks that repos exist already
-      stub_request(:get, "https://testteacher:abc123@api.github.com/repos/testorg/#{username}-testrepo").
-         to_return(:status => 200, :body => "", :headers => {})
-    end
-
-    # create the issue in each repo
-    student_usernames.each do |username|
-      team_id = 0
-      student_teams.each do |st|
-        if st[:name].eql?(username)
-          team_id = st[:id]
-        end
+      request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg',
+        login: 'testorg',
+        url: 'https://api.github.com/orgs/testorg'
+      )
+      request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg/teams?per_page=100', student_teams)
+      student_usernames.each do |username|
+        # Action checks that repos exist already
+        stub_request(:get, "https://testteacher:abc123@api.github.com/repos/testorg/#{username}-testrepo").
+           to_return(:status => 200, :body => "", :headers => {})
       end
-      issue_body = File.read(issue_fixture_path).gsub("\n", "\\n")
-      labels_list = labels.split(",").map(&:strip).to_s.delete(' ')
-      stub_request(:post, "https://testteacher:abc123@api.github.com/repos/testorg/#{username}-testrepo/issues").
-         with(:body => "{\"labels\":#{labels_list},\"title\":\"Issue Test\",\"body\":\"#{issue_body}\"}").
-         to_return(:status => 201)
+
+      # create the issue in each repo
+      student_usernames.each do |username|
+        team_id = 0
+        student_teams.each do |st|
+          if st[:name].eql?(username)
+            team_id = st[:id]
+          end
+        end
+        issue_body = File.read(issue_fixture_path).gsub("\n", "\\n")
+        labels_list = labels.split(",").map(&:strip).to_s.delete(' ')
+        stub_request(:post, "https://testteacher:abc123@api.github.com/repos/testorg/#{username}-testrepo/issues").
+           with(:body => "{\"labels\":#{labels_list},\"title\":\"#{issue_title}\",\"body\":\"#{issue_body}\"}").
+           to_return(:status => 201)
+      end
+
+      teachers_pet(:open_issue,
+        repository: 'testrepo',
+        organization: 'testorg',
+
+        title: issue_title,
+        body: issue_fixture_path,
+        labels: labels,
+
+        students: students_list_fixture_path,
+        instructors: instructors_list_fixture_path,
+
+        username: 'testteacher',
+        password: 'abc123'
+      )
+
+      request_stubs.each do |request_stub|
+        expect(request_stub).to have_been_requested.once
+      end
     end
 
-    action.run
-
-    request_stubs.each do |request_stub|
-      expect(request_stub).to have_been_requested.once
+    it "open issue no labels" do
+      common_test("")
     end
-  end
 
-  it "open issue no labels" do
-    common_test("")
-  end
-
-  it "open issue with labels" do
-    common_test("bug, feature")
+    it "open issue with labels" do
+      common_test("bug, feature")
+    end
   end
 end
