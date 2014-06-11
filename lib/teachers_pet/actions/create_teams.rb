2dc8f452b5fa621920d9e8872b12c2c64003c0ea
Aidan Feldman <afeld@github.com>
2014-06-11 17:44:55 -0400

    split create_teams into smaller methods

--- lib/teachers_pet/actions/create_teams.rb
+++ lib/teachers_pet/actions/create_teams.rb
@@ -12,10 +12,7 @@ module TeachersPet
 
       def create
         self.init_client
-
-        existing = Hash.new
-        teams = @client.organization_teams(@organization)
-        teams.each { |team| existing[team[:name]] = team }
+        existing = self.existing_teams_by_name
 
         puts "\nDetermining which students need teams created..."
         todo = Hash.new
@@ -36,37 +33,50 @@ module TeachersPet
               name: team_name,
               permission: 'push'
             )
-            teams.push(team)
+            existing[team_name] = team
           end
         end
 
         puts "\nAdjusting team memberships"
+        existing.each do |team_name, team|
+          self.add_members(team)
+        end
+      end
+
+      def existing_teams_by_name
+        teams_by_name = Hash.new
+        teams = @client.organization_teams(@organization)
         teams.each do |team|
-          team_members = get_team_member_logins(team[:id])
-          if team[:name].eql?('Owners')
-            puts "*** OWNERS *** - Ensuring instructors are owners"
-            @instructors.keys.each do |instructor|
-              unless team_members.include?(instructor)
-                @client.add_team_member(team[:id], instructor)
-                puts " -> '#{instructor}' has been made an owner for this course"
-              else
-                puts " -> '#{instructor}' is already an owner"
-              end
+          teams_by_name[team[:name]] = team
+        end
+        teams_by_name
+      end
+
+      def add_members(team)
+        team_members = get_team_member_logins(team[:id])
+        if team[:name].eql?('Owners')
+          puts "*** OWNERS *** - Ensuring instructors are owners"
+          @instructors.keys.each do |instructor|
+            unless team_members.include?(instructor)
+              @client.add_team_member(team[:id], instructor)
+              puts " -> '#{instructor}' has been made an owner for this course"
+            else
+              puts " -> '#{instructor}' is already an owner"
             end
-          elsif @students.key?(team[:name])
-            puts "Validating membership for team '#{team[:name]}'"
-            # If there isn't a team member that is the same name as the team, and we already know
-            # there is a student with the same name, add that student to the team.
-            #unless team_members.include?(team[:name])
-            @students[team[:name]].each do |student|
-              puts "  -> Adding '#{team[:name]}' to the team"
-              @client.add_team_member(team[:id], student)
-            end
-            # Originally, instructors were added to the student's team, but that isn't needed
-            # since instructors are addded to the Owners team that can see all repositories.
-          else
-            puts "*** Team name '#{team[:name]}' does not match any students, ignoring. ***"
           end
+        elsif @students.key?(team[:name])
+          puts "Validating membership for team '#{team[:name]}'"
+          # If there isn't a team member that is the same name as the team, and we already know
+          # there is a student with the same name, add that student to the team.
+          #unless team_members.include?(team[:name])
+          @students[team[:name]].each do |student|
+            puts "  -> Adding '#{team[:name]}' to the team"
+            @client.add_team_member(team[:id], student)
+          end
+          # Originally, instructors were added to the student's team, but that isn't needed
+          # since instructors are addded to the Owners team that can see all repositories.
+        else
+          puts "*** Team name '#{team[:name]}' does not match any students, ignoring. ***"
         end
       end
 
