2ad0e7ea4243ab63c8d734217dcc65a595912501
Aidan Feldman <afeld@github.com>
2014-06-11 23:04:56 -0400

    fix create_teams for groups of students

--- lib/teachers_pet/actions/create_teams.rb
+++ lib/teachers_pet/actions/create_teams.rb
@@ -12,20 +12,37 @@ module TeachersPet
 
       def create
         self.init_client
+        self.create_student_teams
+        self.add_instructors_to_owners
+      end
+
+      def create_student_teams
         teams_by_name = self.existing_teams_by_name
 
-        @students.keys.each do |team_name|
-          if teams_by_name[team_name]
+        @students.each do |key, value|
+          if value
+            # Create one team per group of students
+            team_name = key
+            usernames = value
+          else
+            # Create a team with the same name as the student, with that person as the only member
+            team_name = key
+            usernames = [value]
+          end
+
+          team = teams_by_name[team_name]
+          if team
             puts "Team @#{organization}/#{team_name} already exists."
           else
-            teams_by_name[team_name] = self.create_team(team_name)
+            team = self.create_team(team_name)
           end
+          self.add_users_to_team(team, usernames)
         end
+      end
 
-        puts "\nAdjusting team memberships"
-        teams_by_name.each do |team_name, team|
-          self.add_members(team)
-        end
+      def add_instructors_to_owners
+        owners = self.existing_teams_by_name['Owners']
+        self.add_users_to_team(owners, @instructors.keys)
       end
 
       def create_team(name)
@@ -37,34 +54,29 @@ module TeachersPet
       end
 
       def existing_teams_by_name
-        teams_by_name = Hash.new
-        teams = @client.organization_teams(@organization)
-        teams.each do |team|
-          teams_by_name[team[:name]] = team
+        unless @existing_teams_by_name
+          @existing_teams_by_name = Hash.new
+          teams = @client.organization_teams(@organization)
+          teams.each do |team|
+            @existing_teams_by_name[team[:name]] = team
+          end
         end
-        teams_by_name
+
+        @existing_teams_by_name
       end
 
       def add_users_to_team(team, usernames)
-        team_members = get_team_member_logins(team[:id])
-        usernames.each do |username|
-          if team_members.include?(username)
-            puts " -> @#{username} is already on @#{organization}/#{team[:name]}"
-          else
-            @client.add_team_member(team[:id], username)
-            puts " -> @#{username} has been added to @#{organization}/#{team[:name]}"
+        # Minor optimization, mostly for testing
+        if usernames.any?
+          team_members = get_team_member_logins(team[:id])
+          usernames.each do |username|
+            if team_members.include?(username)
+              puts " -> @#{username} is already on @#{organization}/#{team[:name]}"
+            else
+              @client.add_team_member(team[:id], username)
+              puts " -> @#{username} has been added to @#{organization}/#{team[:name]}"
+            end
           end
-        end
-      end
-
-      def add_members(team)
-        team_name = team[:name]
-        if team_name == 'Owners'
-          self.add_users_to_team(team, @instructors.keys)
-        elsif @students.key?(team_name)
-          self.add_users_to_team(team, @students[team_name])
-        else
-          puts "*** Team @#{organization}/#{team_name} does not match any students, ignoring. ***"
         end
       end
 
