da7925d2fd70bf3813277e15609ed70c2cabce3c
Aidan Feldman <afeld@github.com>
2014-06-17 18:58:59 -0400

    test output in integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,6 +6,7 @@ describe 'integration' do
     it "shows the help output" do
       stdout_str, stderr_str, status = Open3.capture3('bin/teachers_pet help')
       expect(stderr_str).to eq('')
+      expect(stdout_str).to include('create_student_teams')
       expect(status).to be_success
     end
   end
