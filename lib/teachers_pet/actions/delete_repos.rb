bd65a2bcc527182d87a467b439a0a815d563f266
efthym <efthym@cs.uoi.gr>
2015-01-27 22:15:57 +0200

    removed travis-auto-testing from branch

---
+++ lib/teachers_pet/actions/delete_repos.rb
@@ -0,0 +1,55 @@
+module TeachersPet
+  module Actions
+    class DeleteRepos < Base
+      def read_info
+        @repository = self.options[:repository]
+        @organization = self.options[:organization]
+      end
+
+      def load_files
+        @students = self.read_students_file
+      end
+
+      def delete
+        # create a repo for each student
+        self.init_client
+
+        org_hash = self.client.organization(@organization)
+        abort('Organization could not be found') if org_hash.nil?
+        puts "Found organization at: #{org_hash[:login]}"
+
+        # Load the teams - there should be one team per student.
+        # Repositories are given permissions by teams
+        org_teams = self.client.get_teams_by_name(@organization)
+        # For each student - create a repository, and give permissions to their "team"
+        # The repository name is teamName-repository
+        puts "\nDeleting students' assignment repositories..."
+        @students.keys.sort.each do |student|
+          unless org_teams.key?(student)
+            puts("  ** ERROR ** - no team for #{student}")
+            next
+          end
+          repo_name = "#{student}-#{@repository}"
+
+          unless self.client.repository?(@organization, repo_name)
+            puts " --> Does not exist, skipping '#{repo_name}'"
+            next
+          end
+
+		  ######################
+		  # Only managed to get this working only with a 
+		  # Personal access token with a 'delete_repo' scope
+		  ######################
+          puts " --> Deleting '#{repo_name}'"
+          self.client.delete_repository("#{@organization}/#{repo_name}")
+        end
+      end
+
+      def run
+        self.read_info
+        self.load_files
+        self.delete
+      end
+    end
+  end
+end
