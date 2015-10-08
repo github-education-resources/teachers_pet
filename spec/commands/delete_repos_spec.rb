be8a8b1ddb3a8ef79a88f53f6bd05649417f0515
Chris Ewald <chrisewald@gmail.com>
2015-10-07 22:11:56 -0400

    rework delete repos specs to be understandable

--- spec/commands/delete_repos_spec.rb
+++ spec/commands/delete_repos_spec.rb
@@ -2,21 +2,14 @@ require 'spec_helper'
 
 describe 'delete_repos' do
   include CommandHelpers
+  include_context "with expected requests"
 
-  def common_test(create_as_public)
-    request_stubs = []
-
-    request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg',
-      login: 'testorg',
-      url: 'https://api.github.com/orgs/testorg'
-    )
-    request_stubs << stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg/teams?per_page=100', student_teams)
+  def stub_student_create_repo_requests(create_as_public)
     student_usernames.each do |username|
       # Check for the repos existing already
       stub_request(:get, "https://testteacher:abc123@api.github.com/repos/testorg/#{username}-testrepo").
         to_return(status: 404)
     end
-
     student_usernames.each do |username|
       # create the repo for test
       team_id = 0
@@ -36,37 +29,56 @@ describe 'delete_repos' do
           name: "#{username}-testrepo"
         }.to_json)
     end
+  end
 
+  def teachers_pet_create_repos!(create_as_public)
     teachers_pet(:create_repos,
       repository: 'testrepo',
       organization: 'testorg',
       public: create_as_public,
-
       students: students_list_fixture_path,
-
       username: 'testteacher',
       password: 'abc123'
     )
+  end
+
+  def stub_get_org_request
+    stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg',
+      login: 'testorg',
+      url: 'https://api.github.com/orgs/testorg'
+    )
+  end
+
+  def stub_get_org_teams_request
+    stub_get_json('https://testteacher:abc123@api.github.com/orgs/testorg/teams?per_page=100', student_teams)
+  end
+
+  before(:each) do
+    @expected_requests << stub_get_org_request
+    @expected_requests << stub_get_org_teams_request
+  end
+
+  it "deletes public repos" do
+    stub_student_create_repo_requests(true)
+    teachers_pet_create_repos!(true)
     teachers_pet(:delete_repos,
       repository: 'testrepo',
       organization: 'testorg',
-
       students: students_list_fixture_path,
-
       username: 'testteacher',
       password: 'abc123'
     )
-
-    request_stubs.each do |request_stub|
-      expect(request_stub).to have_been_requested.at_least_once
-    end
   end
 
-  it "create repos public" do
-    common_test(true)
-  end
-
-  it "create repos private" do
-    common_test(false)
+  it "deletes private repos" do
+    stub_student_create_repo_requests(false)
+    teachers_pet_create_repos!(false)
+    teachers_pet(:delete_repos,
+      repository: 'testrepo',
+      organization: 'testorg',
+      students: students_list_fixture_path,
+      username: 'testteacher',
+      password: 'abc123'
+    )
   end
 end
